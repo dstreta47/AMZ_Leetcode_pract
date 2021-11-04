2c4745ac0835c5b9c20e4d17239c9ccd78fa91c7
Amit Rana <43043638+dstreta47@users.noreply.github.com>
2021-11-03 20:28:25 -0700

    472. Concatenated Words
    472. Concatenated Words

---
+++ Concatenated/concat-Words.py
@@ -0,0 +1,27 @@
+    def findAllConcatenatedWordsInADict(self, words):
+        res = []
+        preWords = set()
+        words.sort(key = len)
+        for word in words:
+            if self.wordBreak(word, preWords):
+                res.append(word)
+            preWords.add(word)
+        
+        return res
+    
+    def wordBreak(self, string, words):
+        if not words:
+            return False
+        
+        dp = [False] * (len(string) + 1)
+        dp[0] = True
+        
+        for i in range(len(string)+1):
+            for j in range(i):
+                ff = dp[j]
+                gg = string[j:i]
+                if dp[j] and string[j:i] in words:
+                    dp[i] = True
+                    break
+        
+        return dp[-1]
